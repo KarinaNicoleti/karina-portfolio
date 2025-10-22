74316f41661a50f6f10b710e51960a6474b2c94d
KARINA APARECIDA MORAES NICOLETI <karinamoraes51@outlook.com>
2025-10-22 05:59:53 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/karina-portfolio/', // nome do repositório no GitHub
+  base: '/karina-portfolio/', // <== importante: nome exato do repositório no GitHub
   plugins: [react()],
-})
+});
+
